8c0519d760a9f88a8aa4b8ff78ec3ea43cfaa7b1
0m3ga13 <barkaoussama13@gmail.com>
2023-09-05 18:05:38 +0100

    done

--- Components/UserCard/UserCard.jsx
+++ Components/UserCard/UserCard.jsx
@@ -1,13 +1,14 @@
-export default function UserCard() {
+export default function UserCard({ data }) {
+  const { name, login, avatar_url } = data;
+
   return (
     <div className="user-card">
       <div>
-        <img alt="avatar" />
+        <img src={avatar_url} alt="avatar" />
       </div>
       <div>
-        <h5> Name:</h5>
-
-        <p> Username: </p>
+        <h5>Name: {name}</h5>
+        <p>Username: {login}</p>
       </div>
     </div>
   );
